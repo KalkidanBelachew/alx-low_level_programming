81650f5e6815a8f6f0da3b78fecb5efe080e29ba
KalkidanBelachew <kal.belachew.18@gmail.com>
2022-03-24 09:35:11 +0300

    More Pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,24 @@
 #include "main.h"
 
 /**
- * *leet - check the code for Holberton School students.
- *
- * @str: string.
- * Return: string.
+ * leet - a function that encodes a string into 1337.
+ * @s: an input string to encode
+ * Return: An encode string
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-	int i;
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
+
+		i++;
 	}
-	return (str);
+
+	return (s);
 }
