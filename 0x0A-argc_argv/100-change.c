0ff2d30e812075010ee3b2330671d2eee9d15981
KalkidanBelachew <kal.belachew.18@gmail.com>
2022-04-01 12:18:16 +0300

    Static

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,40 +1,55 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * numchecker - verifies if string has only numbers
- * @s: string to check
- * Description: return 1 if only numbers in string, 0 if not
- * Return: 1 if only numbers, 0 if not
- **/
-
-int numchecker(char *s)
+ * main - A proogram that prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: The arguements' counter
+ * @argv: The argument's values
+ * Return: 1 if the number of arguments passed is not exactly
+ * or 0 in otherwise
+ */
+int main(int argc, char **argv)
 {
-	int i;
+	int amount, coins = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (argc == 2)
 	{
-		if (s[i] < '0' || s[i] > '9')
+		amount = atoi(argv[1]);
+		if (amount < 0)
+		{
+			printf("%d\n", 0);
 			return (0);
+		}
+		if (amount % 25 >= 0)
+		{
+			coins += amount / 25;
+			amount = amount % 25;
+		}
+		if (amount % 10 >= 0)
+		{
+			coins += amount / 10;
+			amount = amount % 10;
+		}
+		if (amount % 5 >= 0)
+		{
+			coins += amount / 5;
+			amount = amount % 5;
+		}
+		if (amount % 2 >= 0)
+		{
+			coins += amount / 2;
+			amount = amount % 2;
+		}
+		if (amount % 1 >= 0)
+			coins += amount;
+		printf("%d\n", coins);
+		return (0);
 	}
-	return (1);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
-
-/**
- * main - find least amount of coins needed for change
- * @argc: number of parameters entered
- * @argv: strings entered including file name
- * Description: prints the minimum number of coins to make change
- * Return: zero
- **/
-
-int main(int argc, char *argv[])
-{
-	int change, coins;
-	char *input = argv[1];
-
-	change = coins = 0;
-
-	if (argc != 2)
-
