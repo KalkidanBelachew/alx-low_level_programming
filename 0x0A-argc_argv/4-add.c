178f942971a67de346cbff64167219c798017344
KalkidanBelachew <kal.belachew.18@gmail.com>
2022-04-01 12:07:15 +0300

    Static

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,51 +1,30 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * numchecker - verifies if string has only numbers
- * @s: string to check
- * Description: return 1 if only numbers in string, 0 if not
- * Return: 1 if only numbers, 0 if not
- **/
-
-int numchecker(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * main - find least amount of coins needed for change
- * @argc: number of parameters entered
- * @argv: strings entered including file name
- * Description: prints the minimum number of coins to make change
- * Return: zero
- **/
-
+ * main - A program that adds positive numbers
+ * @argc: The arguments' counter
+ * @argv: The argument's values
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
-	int sum, i;
+	int num, result = 0, i;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	while (argc-- > 1)
 	{
-		if (numchecker(argv[i]) == 0)
+		for (i = 0; argv[argc][i]; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		if (atoi(argv[i]) <= 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		num = atoi(argv[argc]);
+		result += num;
 	}
-
+	printf("%d\n", result);
+	return (0);
+}
